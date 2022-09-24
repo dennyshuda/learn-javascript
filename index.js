64180bcb5073a40af4e697ffa6c8683f6e023c32
M Haidar Hanif <git@mhaidarhanif.com>
2022-09-24 21:37:12 +0700

    refactor: repeat attempt to guess the number

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
-const insertName = prompt("Input your name");
-
-if (insertName == "halo" || insertName == "Halo") {
-  window.alert("Nice to meet you!");
-} else {
-  window.alert(`Halo ${insertName} Lets play`);
-}
-
-const fullName = "Denny Setyawan Huda";
-const age = 20;
-
-console.log(fullName, age);
-
-const animals = ["cat", "dog", "cow", "chicken", "bear"];
-
-animals.push("duck");
-animals.forEach((animal, index) => {
-  console.log(`ke-${index} = ${animal}`);
-});
-
-function multipleNumber(number) {
-  let result = number ** 2;
-  return result;
-}
-
-console.log(multipleNumber(9));
-
-function sayName(firstName, lastName = "oke") {
-  console.log(`Hai ${firstName} ${lastName}`);
-}
-
-sayName("akbar");
-
-console.log("Let's Play The game");
-
-let secretNumber = 28;
+console.log("Let's play a number guesssing game");
 
 let chance = 3;
 
-function guessTheNumber(userNumber) {
+for (let attempt = 1; attempt <= chance; attempt++) {
+  let guessedNumber = prompt("Guess the number:");
+  let secretNumber = 28;
+
   if (userNumber != secretNumber) {
     chance = chance - 1;
     console.log(`Opps... Your chance left is ${chance}`);
+    break;
   } else {
     console.log(`Good Job.. the secret number is ${secretNumber}`);
+    break;
   }
 }
-
-guessTheNumber(78);
